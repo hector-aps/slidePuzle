7b890c8927450ff44aed8d34aa76c29998f8e658
héctor peña <121992846+hector-aps@users.noreply.github.com>
2023-01-05 12:45:49 -0600

    Recolor, documentación de funciones.

--- NumPuzle.py
+++ NumPuzle.py
@@ -3,17 +3,23 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.resizable(0, 0)
+root.resizable(1,1)
 root.geometry("400x300")
-root.geometry()
 root.title("NumPuzzle")
-window = Frame()
+window = Frame(root)
+window.config(bg="#F6DDCC")
+root.config(bg="#F6DDCC")
+color = "#F6DDCC"
 
 game = Puzzle()
 
 #----Funcionalidad Interfaz---------------
 
 def actualizar():
+	'''
+	Funcion que actualiza los valores de los elementos de la interfaz grafica a los valores
+	internos del objeto game
+	'''
 	global intentos
 	intentos.config(text = f"Movimientos: {game.intentos}")
 	bot1.set(str(game.tablero[0]))
@@ -28,23 +34,38 @@ def actualizar():
 	comprobar()
 	
 def piezaAct(pieza):
-	game.mover(pieza)
-	actualizar()
+	'''
+	Funcion que utilizan los botones del juego, reciben la pieza que se intenta mover y
+	llama la funcion interna de game para mover la pieza, luego llama a la funcion actualizar()
+	'''
+	if not game.verificar():
+		game.mover(pieza)
+		actualizar()
 
 def mezclar():
+	'''
+	Funcion que llama al metodo revolver() de game y llama a la funcion actualizar(), es utilizada
+	por el boton Mezclar
+	'''
 	game.revolver()
 	actualizar()
 
 def comprobar():
+	'''
+	Verifica mediante el metodo verificar() de game para saber si se ha completado el reto, si es
+	asi nos mostrara un mensaje te completado y se mostrara con cuantos intentos se termino
+	'''
 	if game.verificar():
-		messagebox.showinfo("Listo", f"Completado en {game.intentos} movimientos")
+		messagebox.showinfo("Completado!", f"Completado en {game.intentos} movimientos")
 
 
 #----Interfaz---------------
+#En esta parte se desarrolla la interfaz grafica del juego
 
-Label(window, text = "Numeros", font = ("Comic Sans MS", 12, "bold")).grid(row=0, column=0, columnspan=3, pady=10, padx=10)
 
-intentos = Label(window, text = "Intentos: 0")
+Label(window, text = "Numeros", fg = "#6E2C00", bg = color, font = ("Comic Sans MS", 12, "bold")).grid(row=0, column=0, columnspan=3, pady=10, padx=10)
+
+intentos = Label(window, text = "Intentos: 0", fg = "#6E2C00", font = ("Comic Sans MS", 10, "bold"), bg = color)
 intentos.grid(row=1, column=0, columnspan=3, pady=10)
 
 bot1 = StringVar()
@@ -74,7 +95,7 @@ Button(window, textvariable=bot8, width = 4, height = 2, command = lambda: pieza
 bot9 = StringVar()
 Button(window, textvariable=bot9, width = 4, height = 2, command = lambda: piezaAct(8)).grid(row=4, column=2)
 
-Button(window, text = "Mezclar", command = mezclar).grid(row=5, column=0, columnspan=3, pady = 10)
+Button(window, text = "Mezclar", fg = "#6E2C00", command = mezclar).grid(row=5, column=0, columnspan=3, pady = 10)
 
 mezclar()
 actualizar()
--- game_class.py
+++ game_class.py
@@ -2,6 +2,11 @@ from random import choice
 
 class Puzzle:
 	def __init__(self):
+		'''
+		Funcion constructora de clase, establece los intentos(movimientos), las posiciones iniciales
+		de las fichas en el tablero, y los posibles movimientos de las piezas dependiendo de
+		la posicion de la pieza vacia.
+		'''
 		self.intentos = 0
 		
 		self.tablero = [1, 2, 3, 4, 5, 6, 7, 8, " "]
@@ -19,16 +24,28 @@ class Puzzle:
 		}
 		
 	def mover(self, pieza):
+		'''
+		Recibe la pieza que se intenta mover y verifica si esta esta entre los posibles movimientos
+		de la pieza vacia, si es asi procede a cambiar las piezas de posicion y agregar un movimiento
+		al contador
+		'''
 		if pieza in self.movPos[self.tablero.index(" ")]:
 			self.tablero[self.tablero.index(" ")] = self.tablero[pieza]
 			self.tablero[pieza] = " "
 			self.intentos +=1
 		
 	def revolver(self):
+		'''
+		Funcion encargada de revolver el tablero realizando 100 movimientos aleatorios y reestableciendo
+		el contador de intentos a 0
+		'''
 		for i in range(100):
 			var = choice(self.movPos[self.tablero.index(" ")])
 			self.mover(var)
 		self.intentos = 0
 
 	def verificar(self):
+		'''
+		Esta funcion se encarga de verificar si el tablero ha sido completado, en cuyo caso retorna True
+		'''
 		return True if self.tablero == [1, 2, 3, 4, 5, 6, 7, 8, " "] else False
